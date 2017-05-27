e290c8b8dcb3bd71ab28136deff85bf6d2682191
Daniel Schultheis <d.schultheis@kabel-salat.net>
2017-05-28 01:24:40 +0200

    Updated configuration file

--- config/app.default.php
+++ config/app.default.php
@@ -14,20 +14,24 @@ return [
     /**
      * Configure basic information about the application.
      *
-     * - uuidRegexp - Regular expression used to validate the rfid uuid
-     * - lastIdFile - Location of the file that stores the last recognized uuid
+     * - uuidRegexp      - Regular expression used to validate the rfid uuid
+     * - audioFileRegexp - Regular expression used to filter out audio files from rsync output
+     *                     used to build the playlist file.
+     * - lastIdFile      - Location of the file that stores the last recognized uuid
      */
     'App' => [
         'uuidRegexp' => '/^[0-9a-f]{8}$/',
+        'audioFileRegexp' => '/\.aac|\.aax|\.ape|\.flac|\.m4a|\.mp3|\.ogg|\.wav|\.wma$/',
         'lastIdFile' => ROOT . DS . 'last_id',
     ],
 
     /**
-     * Configuration of the remote server that is queried to receive the playlists
+     * Configuration of the remote server that is queried to receive
+     * the playlists.
      *
      * - protocol - Which protocol to use (http or https)
-     * - host - Hostname or IP-Adress of the remote host
-     * - port - Remote port
+     * - host     - Hostname or IP-Adress of the remote host
+     * - port     - Remote port
      */
     'Server' => [
         'protocol' => 'http',
@@ -37,14 +41,31 @@ return [
 
     /**
      * Rsync configuration
+     *
+     * - source         - Source host
+     * - sourceUsername - Username used for ssh login
+     * - target         - Target folder
+     *
+     * !!DO NOT!! change anything else unless you know exactly what
+     * you are doing. All rsync options and arguments have been
+     * carefully set up to be most compatible.
+     *
+     * The filter is used to filter out the @eaDir directories from
+     * my Synology DiskStation.
+     *
+     * If you want to add additional audio files, add them to the
+     * include-section. Please make sure to add them to the
+     * App.audioFileRegexp configurion as well.
+     * Otherwise the audio files will be downloaded but not added
+     * to the playlist.
      */
     'Rsync' => [
-        'source' => '~/Musik/Ripped',
-        // 'sourceUsername' => 'dachande',
-        'target' => '/tgramp/foo',
+        'source' => 'remote.hostname:/',
+        'sourceUsername' => 'username',
+        'target' => '~/t00niebox/music',
         // 'targetUsername' => 'dachande',
         'config' => [
-            'options' => ['v', 'z', 'P', 'R'],
+            'options' => ['v', 'z', 'r', 'P', 'R'],
             'arguments' => [
                 'delete' => true,
                 'delete-before' => true,
@@ -69,10 +90,24 @@ return [
         ],
     ],
 
+    /**
+     * MPD configuration
+     *
+     * - host      - Hostname of your MPD server. Should be set to localhost unless the
+     *               MPD server is on a different machine.
+     * - port      - Port of your MPD server.
+     * - playlists - Path where MPD stores its playlists
+     */
     'Mpd' => [
-        'playlists' => '/tmp',
+        'host' => 'localhost',
+        'port' => 6600,
+        'password' => 't00niebox',
+        'playlists' => '/home/pi/.mpd/playlists',
     ],
 
+    /**
+     * Logging configuration
+     */
     'Log' => [
         'debug' => [
             'className' => 'Cake\Log\Engine\ConsoleLog',
@@ -80,6 +115,9 @@ return [
         ],
     ],
 
+    /**
+     * Console colors
+     */
     'Console' => [
         'styles' => [
             'debug' => ['text' => 'magenta'],
